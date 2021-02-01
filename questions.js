ab5533edad40786d909ffd629b58ece41e0b6944
Dhineshkumar1999 <dhieshkumar83@gmail.com>
2021-02-01 12:27:17 +0530

    first commit

---
+++ questions.js
@@ -0,0 +1,118 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which program is used by web clients to view the web pages?",
+    answer: "Web browser",
+    options: [
+      "Web browser",
+      "Search Engine",
+      "Web server",
+      "Protocol"
+    ]
+  },
+    {
+    numb: 2,
+    question: "www is based on which model?",
+    answer: "Client-server",
+    options: [
+      "Local-server",
+      "Client-server",
+      "3-tier",
+      "None of these"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which language is used for creating Web Pages?",
+    answer: "HTML",
+    options: [
+      "PASCAL",
+      "C",
+      "HTML",
+      "BASIC"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  
+     {
+     numb: 6,
+     question: "What is the abbreviation of HTTP?",
+     answer: "Hyper Text Transfer Protocol",
+     options: [
+       "Hypertext tag path",
+       "Hyper Text Transfer Protocol",
+       "Hypertext transfer path",
+       "None"
+     ]
+   },
+
+   {
+    numb: 7,
+    question: "The correct sequence of HTML tags for starting a webpage is - ",
+    answer: "HTML, Head, Title, Body",
+    options: [
+      "Head, Title, HTML, body",
+      "HTML, Body, Title, Head",
+      "HTML, Head, Title, Body",
+      "HTML, Title, Head, Body"
+    ]
+  },
+
+  {
+    numb: 8,
+    question: "Transferring your HTML code from one machine to server is known as ___",
+    answer: "Hosting",
+    options: [
+        "Hosting",
+        "Indexing",
+        "Serving",
+        "Sorting"
+    ]
+  },
+
+  {
+    numb: 9,
+    question: "The primary colors are ____",
+    answer: "Red, green, blue",
+    options: [
+        "Red, green, yellow",
+        "Red, white, yellow",
+        "Red, green, blue",
+        "Red, yellow, blue"
+    ]
+  },
+
+  {
+    numb: 10,
+    question: "GIF is the abbreviation for ___",
+    answer: "Graphics Interchange Format",
+    options: [
+      "Graphics Interchange Format",
+      "Graphics Instruction Format",
+      "Graphics Item Format",
+      "Graphics Information Format"
+    ]
+  },
+];
